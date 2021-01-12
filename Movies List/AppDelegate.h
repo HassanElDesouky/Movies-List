29818169e9cf46b1a78a7f671eff12c13d151d24
Hassan <hassan.ahmed1991999@gmail.com>
2021-01-12 03:19:58 +0200

    [Git] First commit

---
+++ Movies List/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  Movies List
+//
+//  Created by Hassan El Desouky on 12/01/2021.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ Movies List/SceneDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SceneDelegate.h
+//  Movies List
+//
+//  Created by Hassan El Desouky on 12/01/2021.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
